f674a1629c68f79637228bbbf1d99f3d87bbe70e
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2016-12-20 13:19:26 +0100

    MVC 2.0

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ if(!isset($_GET['controller'])){
 switch ($controller) {	
 	case "etudiant" :
 		if(!isset($_GET['action'])){
-			$_GET['action'] = "Accueil";
+			$_GET['action'] = "accueil";
 		}
 		require ("{$ROOT}{$DS}controller{$DS}controller".ucfirst($controller).".php"); //ucfirst met la premiere lettre de la chaine en MAJ
 		break;
