fb703817ff27e5da62d38c0ddbeb113ef880afa7
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2016-12-20 12:30:21 +0100

    header modifs

--- view/header.php
+++ view/header.php
@@ -1,3 +1,13 @@
+<!--
+Verifie quelle page est active
+-->
+<?php 
+$directoryURI = $_SERVER['REQUEST_URI'];
+$path = parse_url($directoryURI, PHP_URL_PATH);
+$components = explode('/', $path);
+$first_part = $components[1];
+?>
+
 <!-- Tête de page -->
 <div class="navbar">
   <div class="navbar-inner">
@@ -5,11 +15,11 @@
       <a href="./index.php"><img src="img/polytech.png" alt=""></a>
       <ul class="nav nav-collapse pull-right">
         <?php if(!empty($_SESSION['nom'])){ ?>
-          <li><a href="index.html" class="active"><i class="icon-user"></i> Votre Profil</a></li>
-          <li><a href="skills.html"><i class="icon-trophy"></i> Personnalité</a></li>
-          <li><a href="resume.html"><i class="icon-doc-text"></i> Test de personnalité</a></li>
+          <li class="<?php if ($first_part==" Votre Profil") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=profil"><i class="icon-user"></i> Votre Profil</a></li>
+          <li class="<?php if ($first_part==" Personnalité") {echo "active"; }?>"><a href="skills.html"><i class="icon-trophy"></i> Personnalité</a></li>
+          <li class="<?php if ($first_part==" Test de personnalité") {echo "active"; }?>"><a href="resume.html"><i class="icon-doc-text"></i> Test de personnalité</a></li>
         <?php } ?>
-          <li><a href="contact.html"><i class="icon-mail"></i> Contactez-nous</a></li>
+          <li class="<?php if ($first_part=="") {echo "active"; }?>"><a href="contact.html"><i class="icon-mail"></i> Contactez-nous</a></li>
       </ul>
       <div class="nav-collapse collapse"></div>
     </div>
