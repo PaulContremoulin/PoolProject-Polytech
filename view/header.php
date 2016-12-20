bcc783346e9198d0aaea740763ac4dd5393c4c9f
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2016-12-20 13:36:21 +0100

    power

--- view/header.php
+++ view/header.php
@@ -18,9 +18,9 @@ $first_part = $components[1];
           <li class="<?php if ($first_part==" Votre Profil") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=profil"><i class="icon-user"></i> Votre Profil</a></li>
           <li class="<?php if ($first_part==" Personnalité") {echo "active"; }?>"><a href="skills.html"><i class="icon-trophy"></i> Personnalité</a></li>
           <li class="<?php if ($first_part==" Test de personnalité") {echo "active"; }?>"><a href="resume.html"><i class="icon-doc-text"></i> Test de personnalité</a></li>
-          <li class="<?php if ($first_part==" Se déconnecter") {echo "active"; }?>"><a href="resume.html"><i class="icon-off"></i>  Se déconnecter</a></li>
+          <li class="<?php if ($first_part==" Se déconnecter") {echo "active"; }?>"><a href="resume.html"><i class="icon-power-off"></i>  Se déconnecter</a></li>
         <?php } else { ?>
-          <li class="<?php if ($first_part==" Se connecter") {echo "active"; }?>"><a href="resume.html"><i class="icon-off"></i> Se connecter</a></li>
+          <li class="<?php if ($first_part==" Se connecter") {echo "active"; }?>"><a href="resume.html"><i class="icon-power-off"></i> Se connecter</a></li>
         <?php } ?>
           <li class="<?php if ($first_part==" Contactez-nous") {echo "active"; }?>"><a href="contact.html"><i class="icon-mail"></i> Contactez-nous</a></li>
       </ul>
