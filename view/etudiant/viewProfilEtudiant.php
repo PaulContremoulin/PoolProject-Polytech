8c00f95f98658343a01cd0227d668812779fcf53
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2016-12-26 23:10:34 +0100

    ChartJS

--- view/etudiant/viewProfilEtudiant.php
+++ view/etudiant/viewProfilEtudiant.php
@@ -1,7 +1,9 @@
 <div class="container profile">
+	<!--
 	<div class="span3">
 		<img src="img/mini.png" alt="">
 	</div>
+	-->
 	<div class="span6">
 		<?php if(empty($_SESSION['login'])){ ?> <!-- si l'utilisateur n'est pas encore connecté -->
 			<h1>Bienvenue !</h1>
@@ -25,8 +27,49 @@
 
 		<?php }else{ ?>
 			<h1>Bienvenue <?php print_r($_SESSION['nom']) ?> sur votre test de personnalité</h1>
-			<!-- a completer avec chart js -->
+			<canvas id="myChart" width="400" height="400"></canvas>
 		<?php } ?>
 	</div>
 </div>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<script>
+	var ctx = document.getElementById("myChart");
+	var data = {
+	    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+	    datasets: [
+	        {
+	            label: "My First dataset",
+	            backgroundColor: "rgba(179,181,198,0.2)",
+	            borderColor: "rgba(179,181,198,1)",
+	            pointBackgroundColor: "rgba(179,181,198,1)",
+	            pointBorderColor: "#fff",
+	            pointHoverBackgroundColor: "#fff",
+	            pointHoverBorderColor: "rgba(179,181,198,1)",
+	            data: [65, 59, 90, 81, 56, 55, 40]
+	        },
+	        {
+	            label: "My Second dataset",
+	            backgroundColor: "rgba(255,99,132,0.2)",
+	            borderColor: "rgba(255,99,132,1)",
+	            pointBackgroundColor: "rgba(255,99,132,1)",
+	            pointBorderColor: "#fff",
+	            pointHoverBackgroundColor: "#fff",
+	            pointHoverBorderColor: "rgba(255,99,132,1)",
+	            data: [28, 48, 40, 19, 96, 27, 100]
+	        }
+	    ]
+	};
+	var myChart = new Chart(ctx, {
+	    type: "radar",
+	    data: data,
+	    options: {
+	            scale: {
+	                reverse: true,
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }
+	    }
+	});
+</script>
--- view/header.php
+++ view/header.php
@@ -17,11 +17,8 @@ $first_part = $components[1];
       <ul class="nav nav-collapse pull-right">
         <?php if(!empty($_SESSION['login']) && isset($_SESSION['login'])){ ?>
           <li class="<?php if ($action=="profil") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=profil"><i class="icon-user"></i> Votre Profil</a></li>
-          <li class="<?php if ($first_part==" Personnalité") {echo "active"; }?>"><a href="skills.html"><i class="icon-trophy"></i> Personnalité</a></li>
           <li class="<?php if ($first_part==" Test de personnalité") {echo "active"; }?>"><a href="resume.html"><i class="icon-doc-text"></i> Test de personnalité</a></li>
-          <li class="<?php if ($first_part==" Se déconnecter") {echo "active"; }?>"><a href="resume.html"><i class="icon-power"></i>  Se déconnecter</a></li>
-        <?php } else { ?>
-          <li class="<?php if ($first_part==" Se connecter") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=connexion"><i class="icon-power"></i> Se connecter</a></li>
+          <li class="<?php if ($action=="deconnexion") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=deconnexion"><i class="icon-power"></i>  Se déconnecter</a></li>
         <?php } ?>
       </ul>
       <div class="nav-collapse collapse"></div>
