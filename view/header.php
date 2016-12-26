a2cf093566336a14482dd4690a4013ed9d40ca12
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2016-12-26 22:51:24 +0100

    activebar

--- view/header.php
+++ view/header.php
@@ -16,7 +16,7 @@ $first_part = $components[1];
       <a href="./index.php"><img src="img/polytech.png" alt=""></a>
       <ul class="nav nav-collapse pull-right">
         <?php if(!empty($_SESSION['login']) && isset($_SESSION['login'])){ ?>
-          <li class="<?php if ($first_part==" Votre Profil") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=profil"><i class="icon-user"></i> Votre Profil</a></li>
+          <li class="<?php if ($action=="profil") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=profil"><i class="icon-user"></i> Votre Profil</a></li>
           <li class="<?php if ($first_part==" Personnalité") {echo "active"; }?>"><a href="skills.html"><i class="icon-trophy"></i> Personnalité</a></li>
           <li class="<?php if ($first_part==" Test de personnalité") {echo "active"; }?>"><a href="resume.html"><i class="icon-doc-text"></i> Test de personnalité</a></li>
           <li class="<?php if ($first_part==" Se déconnecter") {echo "active"; }?>"><a href="resume.html"><i class="icon-power"></i>  Se déconnecter</a></li>
