bc4221057793fca15008bef73476d4df293f40a1
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-03 17:00:09 +0100

    test debug

--- view/etudiant/viewTestEtudiant.php
+++ view/etudiant/viewTestEtudiant.php
@@ -1,7 +1,7 @@
 <div class="container profile">
 	<div class="span6">
 		<form method="POST" action="index.php?controller=etudiant&amp;action=test&amp;option=null">
-			<h3>Groupe n°<?php echo "$groupe->getIdGroupe()"; ?></h3>
+			<h3>Groupe n°<?php echo "$groupe->getIdGroupe();"; ?></h3>
 			<fieldset>
 				<?php foreach ($tab_answers as $values) { ?>
 					<label for="<?php echo "$values[idr]"; ?>"><?php echo "$values[libelle]"; ?></label>
