e916229db2f105aff5c7b21036a4fc8b4b4eb2ca
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-03 17:18:04 +0100

    formualire test

--- view/etudiant/viewTestEtudiant.php
+++ view/etudiant/viewTestEtudiant.php
@@ -4,9 +4,9 @@
 			<h3>Groupe n°<?php echo "$idGroupe"; ?> </h3>
 			<fieldset>
 				<?php foreach ($tab_answers as $values) { ?>
-					<label for="<?php echo "$values[idr]"; ?>"><?php echo "$values[libelle]"; ?></label>
+					<label for="<?php echo "$values[idr]"; ?>"><?php echo "$values[libelle]"; ?></label> <INPUT type= "radio" name="choix1" value="<?php echo "$values[idp]"; ?>"> 1er choix <INPUT type= "radio" name="choix2" value="<?php echo "$values[idp]"; ?>"> 2eme choix <INPUT type= "radio" name="choix3" value="<?php echo "$values[idp]"; ?>"> 3eme choix
 				<?php } ?>
-				<input id="submit" class="btn btn-large" type="submit" value="Connexion" />
+				<input id="submit" class="btn btn-large" type="submit" value="Validez" />
 			</fieldset> 
 		</form>
 	</div>
