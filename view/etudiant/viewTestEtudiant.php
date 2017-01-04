4eab0c9cd05e27f650a978d55894c16b20824a23
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-04 10:43:22 +0100

    tableau test

--- view/etudiant/viewTestEtudiant.php
+++ view/etudiant/viewTestEtudiant.php
@@ -2,12 +2,23 @@
 	<div class="span6">
 		<form method="POST" action="index.php?controller=etudiant&amp;action=test&amp;option=null">
 			<h3>Groupe n°<?php echo "$idGroupe"; ?> </h3>
-			<fieldset>
+				<table>
+					<tr>
+						<th></th>
+						<th>1</th>
+						<th>2</th>
+						<th>3</th>
+					</tr>
 				<?php foreach ($tab_answers as $values) { ?>
-					<p><?php echo "$values[libelle]"; ?><INPUT type= "radio" name="choix1" value="<?php echo "$values[idp]"; ?>"> 1er choix <INPUT type= "radio" name="choix2" value="<?php echo "$values[idp]"; ?>"> 2eme choix <INPUT type= "radio" name="choix3" value="<?php echo "$values[idp]"; ?>"> 3eme choix </p>
+					<tr>
+						<td><?php echo "$values[libelle]"; ?></td>
+						<td><INPUT type= "radio" name="choix1" value="<?php echo "$values[idp]"; ?>"></td>
+						<td><INPUT type= "radio" name="choix2" value="<?php echo "$values[idp]"; ?>"></td>
+						<td><INPUT type= "radio" name="choix3" value="<?php echo "$values[idp]"; ?>"></td>
+					</tr>
 				<?php } ?>
+				</table>
 				<input type="submit" class="btn btn-large" name="Precedent" value="Precedent" /><input type="submit" class="btn btn-large" name="Suivant" value="Suivant" />
-			</fieldset> 
 		</form>
 	</div>
 </div>
