ad4dcfc459cbc74044aa0dae7a43a78eb959d497
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-04 12:00:01 +0100

    maj boutons active

--- view/header.php
+++ view/header.php
@@ -5,9 +5,9 @@
       <a href="./index.php"><img src="img/polytech.png" alt=""></a>
       <ul class="nav nav-collapse pull-right">
         <?php if(!empty($_SESSION['login']) && isset($_SESSION['login'])){ ?>
-          <li class="<?php if ($action=="profil") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=profil"><i class="icon-user"></i> Votre Profil</a></li>
-          <li class="<?php if ($action=="test") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=test"><i class="icon-doc-text"></i> Test de personnalité</a></li>
-          <li class="<?php if ($action=="deconnexion") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=deconnexion"><i class="icon-power"></i>  Se déconnecter</a></li>
+          <li class="<?php if ($view=="profil") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=profil"><i class="icon-user"></i> Votre Profil</a></li>
+          <li class="<?php if ($view=="test") {echo "active"; }?>"><a href="index.php?controller=etudiant&amp;action=test"><i class="icon-doc-text"></i> Test de personnalité</a></li>
+          <li><a href="index.php?controller=etudiant&amp;action=deconnexion"><i class="icon-power"></i>  Se déconnecter</a></li>
         <?php } ?>
       </ul>
       <div class="nav-collapse collapse"></div>
