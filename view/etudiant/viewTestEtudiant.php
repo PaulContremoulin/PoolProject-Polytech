7395f047eeb1a7c74ef3dc2329443f58fb084705
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-04 10:22:41 +0100

    Formulaire de test

--- view/etudiant/viewTestEtudiant.php
+++ view/etudiant/viewTestEtudiant.php
@@ -4,9 +4,9 @@
 			<h3>Groupe n°<?php echo "$idGroupe"; ?> </h3>
 			<fieldset>
 				<?php foreach ($tab_answers as $values) { ?>
-					<label for="<?php echo "$values[idr]"; ?>"><?php echo "$values[libelle]"; ?></label> <INPUT type= "radio" name="choix1" value="<?php echo "$values[idp]"; ?>"> 1er choix <INPUT type= "radio" name="choix2" value="<?php echo "$values[idp]"; ?>"> 2eme choix <INPUT type= "radio" name="choix3" value="<?php echo "$values[idp]"; ?>"> 3eme choix
+					<p><?php echo "$values[libelle]"; ?><INPUT type= "radio" name="choix1" value="<?php echo "$values[idp]"; ?>"> 1er choix <INPUT type= "radio" name="choix2" value="<?php echo "$values[idp]"; ?>"> 2eme choix <INPUT type= "radio" name="choix3" value="<?php echo "$values[idp]"; ?>"> 3eme choix </p>
 				<?php } ?>
-				<input id="submit" class="btn btn-large" type="submit" value="Validez" />
+				<input type="submit" class="btn btn-large" name="Precedent" value="Precedent" /><input type="submit" class="btn btn-large" name="Suivant" value="Suivant" />
 			</fieldset> 
 		</form>
 	</div>
