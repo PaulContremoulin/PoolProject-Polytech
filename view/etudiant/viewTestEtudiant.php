565fa6d5fd5272237b617461c210d5d5fd0113b7
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-10 16:07:46 +0100

    modif chart

--- view/etudiant/viewTestEtudiant.php
+++ view/etudiant/viewTestEtudiant.php
@@ -15,6 +15,7 @@
 					<tr>
 						<td><?php echo "$values[libelle]"; ?></td>
 						<td><INPUT type= "radio" name="choix1" value="<?php echo "$values[idp]"; ?>"></td>
+						<td><INPUT type= "radio" name="choix2" value="<?php echo "$values[idp]"; ?>"></td>
 						<td><INPUT type= "radio" name="choix3" value="<?php echo "$values[idp]"; ?>"></td>
 					</tr>
 				<?php } ?>
