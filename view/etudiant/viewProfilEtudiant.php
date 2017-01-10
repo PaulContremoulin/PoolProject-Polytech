263f2c6b1a6b994996d1c7cc2189775069f5589d
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-10 18:22:30 +0100

    modif chart

--- view/etudiant/viewProfilEtudiant.php
+++ view/etudiant/viewProfilEtudiant.php
@@ -47,6 +47,13 @@
 				    type: "radar",
 				    data: data,
 				    options: {
+				    	legend: {
+				    		display: true,
+            				labels: {
+                				fontColor: 'rgb(255, 99, 132)'
+                				fontSize: '10'
+            				}
+				    	}
 				            scale: {
 				                ticks: {
 				                    beginAtZero: true
