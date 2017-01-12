56e351f266b1686bffcd28e2fce52d1fd7fb1a58
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-12 14:50:32 +0100

    modif visuel

--- view/etudiant/viewProfilEtudiant.php
+++ view/etudiant/viewProfilEtudiant.php
@@ -10,8 +10,8 @@
 			require_once("viewConnexionEtudiant.php");
 		}else{ ?>
 			<h2>Votre test de personnalité à donné le résultat suivant</h2>	
-		<div id="mychart1">	
-			<canvas id="myChart" width="400" height="400" ></canvas>
+		<div>	
+			<canvas id="myChart" width="80%" height="80%" ></canvas>
 
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 			<script>
