3dc67ee505c14d04db09733f6243656d5c91e688
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-13 15:10:45 +0100

    Mise à jour Profil

--- model/modelProfil.php
+++ model/modelProfil.php
@@ -7,13 +7,15 @@ class ModelProfil extends Model {
 
 	private $id_profil;
 	private $libelle_profil;
+  private $description_profil;
 	protected static $table = 'Profil';
   	protected static $primary = 'id_profil';
 
-  	public function __construct($idp = NULL, $lib = NULL){
-      if (!is_null($idp) && !is_null($lib)){
+  	public function __construct($idp = NULL, $lib = NULL, $descrip = NULL){
+      if (!is_null($idp) && !is_null($lib) && !is_null($descrip)){
         $this->id_profil = $idp;
         $this->libelle_profil = $lib;
+        $this->description_profil = $descrip;
       }
   	}
 
@@ -24,6 +26,10 @@ class ModelProfil extends Model {
   	public function get_libelle(){
   		return $this->libelle_profil;
   	}
+
+    public function get_description(){
+      return $this->description_profil;
+    }
 
     public static function retrieve_id2($libelle){
       $sql = "SELECT id_profil FROM ".static::$table." WHERE libelle_profil LIKE '".$libelle."';";
