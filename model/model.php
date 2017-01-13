bab5f9e414f5121f63e7be8d65fb59b30d60c383
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-13 11:38:43 +0100

    Mise à jour des fonctions génériques dans model.php

--- model/model.php
+++ model/model.php
@@ -71,6 +71,114 @@ class Model{
 		}
   	}
 
+	public static function getAll(){
+	    $SQL="SELECT * FROM ".static::$table.";";
+	    try{
+	 		$rep = Model::$pdo->query($SQL);
+	    	$rep->setFetchMode(PDO::FETCH_CLASS, 'Model'.static::$table);
+	    	return $rep->fetchAll();
+	    } catch(PDOException $e) {
+			if (Conf::getDebug()) {
+				echo $e->getMessage(); // affiche un message d'erreur
+			} else {
+				echo 'Une erreur est survenue <a href="www.google.com"> retour a la page d\'accueil </a>';
+			}
+			die();
+		}   
+    }
+
+
+    public static function getAllOrder($att,$order){
+	    $SQL="SELECT * FROM ".static::$table." ORDER BY ".$att." ".$order.";";
+	    try{
+	 		$rep = Model::$pdo->query($SQL);
+	    	$rep->setFetchMode(PDO::FETCH_CLASS, 'Model'.static::$table);
+	    	return $rep->fetchAll();
+	    } catch(PDOException $e) {
+			if (Conf::getDebug()) {
+				echo $e->getMessage(); // affiche un message d'erreur
+			} else {
+				echo 'Une erreur est survenue <a href="www.google.com"> retour a la page d\'accueil </a>';
+			}
+			die();
+		}   
+    }
+
+	public static function getAll(){
+	    $SQL="SELECT * FROM ".static::$table.";";
+	    try{
+	 		$rep = Model::$pdo->query($SQL);
+	    	$rep->setFetchMode(PDO::FETCH_CLASS, 'Model'.static::$table);
+	    	return $rep->fetchAll();
+	    } catch(PDOException $e) {
+			if (Conf::getDebug()) {
+				echo $e->getMessage(); // affiche un message d'erreur
+			} else {
+				echo 'Une erreur est survenue <a href="www.google.com"> retour a la page d\'accueil </a>';
+			}
+			die();
+		}   
+    }
+
+    public static function getAllOrder($att,$order){
+	    $SQL="SELECT * FROM ".static::$table." ORDER BY ".$att." ".$order.";";
+	    try{
+	 		$rep = Model::$pdo->query($SQL);
+	    	$rep->setFetchMode(PDO::FETCH_CLASS, 'Model'.static::$table);
+	    	return $rep->fetchAll();
+	    } catch(PDOException $e) {
+			if (Conf::getDebug()) {
+				echo $e->getMessage(); // affiche un message d'erreur
+			} else {
+				echo 'Une erreur est survenue <a href="www.google.com"> retour a la page d\'accueil </a>';
+			}
+			die();
+		}   
+    }
+
+	function delete($para) {
+	$sql = "DELETE FROM ".static::$table." WHERE ".static::$primary."=:nom_var";
+	try{
+	  $req_prep = Model::$pdo->prepare($sql);
+	  $req_prep->bindParam(":nom_var", $para);
+	  $req_prep->execute();
+	  return 0;
+	} catch(PDOException $e) {
+	  if (Conf::getDebug()) {
+	    echo $e->getMessage(); // affiche un message d'erreur
+	  }
+	  return -1;
+	  die();
+	}
+	}
+
+	function update($tab, $old) {
+		$sql = "UPDATE ".static::$table." SET";
+		foreach ($tab as $cle => $valeur){
+			$sql .=" ".$cle."=:new".$cle.",";
+		}
+		$sql=rtrim($sql,",");
+		$sql.=" WHERE ".static::$primary."=:oldid;";
+		try{
+		  $req_prep = Model::$pdo->prepare($sql);
+		  $values = array();
+		  foreach ($tab as $cle => $valeur){
+		  		$values[":new".$cle] = $valeur;
+		  }
+		  $values[":oldid"] = $old;
+		  $req_prep->execute($values);
+		  $obj = Model::select($tab[static::$primary]);
+		  return $obj;
+		} catch(PDOException $e) {
+		  if (Conf::getDebug()) {
+		    echo "PROBLEME"; // affiche un message d'erreur
+		  }
+		  return -1;
+		  die();
+		}
+	}
+
+
 }
 
 Model::Init();
