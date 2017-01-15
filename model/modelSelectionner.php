20b8e85f7bd4a03aa12b89437b7a1285de65b77e
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-15 03:09:41 +0100

    ajout code promo

--- model/modelSelectionner.php
+++ model/modelSelectionner.php
@@ -151,6 +151,7 @@ class ModelSelectionner extends Model {
       $tab_resultats_promo = array("realiste"=>0 ,"investigatif"=>0 ,"artistique" => 0, "social" => 0, "entrepreneur" => 0, "conventionnel" => 0);
         foreach ($liste_etudiants as $etudiant) {
           $tab_reponse = ModelSelectionner::select_by_num_user($etudiant);
+          print($tab_reponse);
           $tab_intermediaire =  ModelSelectionner::calcul_result_etud($tab_reponse);
           $tab_resultats_promo["realiste"] = $tab_resultats_promo["realiste"] + $tab_intermediaire["realiste"];
           $tab_resultats_promo["investigatif"] = $tab_resultats_promo["investigatif"] + $tab_intermediaire["investigatif"];
