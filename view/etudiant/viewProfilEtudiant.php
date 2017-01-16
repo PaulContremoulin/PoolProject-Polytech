1e3b472d1750bcb4335aca6335926b244deafd2d
AgbodjogbeYves-alain <agboniel@yahoo.fr>
2017-01-16 21:39:41 +0100

    pdf

--- view/etudiant/viewProfilEtudiant.php
+++ view/etudiant/viewProfilEtudiant.php
@@ -22,8 +22,8 @@
 								echo "\"Envoyer mes résultats par mail\"";
 							}?>
 					>
-				</form> 
-				<input type="button" class="btn btn-dark btn-lg" onclick="location.href='../attributs.pdf';" target="_blank" value="Plus de détails"/>
+				</form></br>
+				<input type="button" class="btn btn-dark btn-lg" onclick="location.href='../attributs.pdf';" TARGET="_BLANK" value="Plus de détails"/>
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 				<script>
 					var var_labels = <?php echo json_encode($labels); ?>;
