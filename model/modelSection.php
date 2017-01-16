677afc678bc7917e39a358d40cda790c2359047b
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-16 18:22:42 +0100

    Correction de bogues sur sessions

--- model/modelSection.php
+++ model/modelSection.php
@@ -28,6 +28,50 @@ class ModelSection extends Model {
     }
   }
 
+
+  public static function listeSections(){
+    $sql =  "SELECT id_promo, annee, Section.id_section AS id_section, libelle_section ".
+            "FROM ".static::$table.", Promo ".
+            "WHERE Section.id_section = Promo.id_section ".
+            "ORDER BY Section.id_section ASC, annee ASC;";
+    try{
+
+      $req_prep = Model::$pdo->prepare($sql);
+      $req_prep->execute();
+
+      $temoin_s = "id_sec";
+      $idp = 0;
+      $sections = array();
+
+      while($ligne = $req_prep->fetch(PDO::FETCH_ASSOC)){
+
+        $s = $ligne['id_section'];
+        $p = $ligne['id_promo'];
+
+        if($temoin_s != $s){
+          $sections[$s] = array();
+          $sections[$s][0] = $ligne['libelle_section'];
+          $sections[$s][1] = array();
+          $sections[$s][2] = array();
+          $temoin_s = $s;
+          $idp = 0;
+        }
+
+        $sections[$s][1][$idp] = $p;
+        $sections[$s][2][$idp] = $ligne['annee'];
+        $idp++;
+      }
+
+      return $sections;
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
+
+  }
+
+  
+/*
   public static function listeSections(){
     $sql =  "SELECT id_promo, annee Section.id_section AS id_section, libelle_section ".
             "FROM ".static::$table.", Promo ".
@@ -68,12 +112,7 @@ class ModelSection extends Model {
     }
 
   }
-
-/*
-  function delete(){
-    Model::delete($this->modele);
-  }
-  */
+*/
 
 }
 
