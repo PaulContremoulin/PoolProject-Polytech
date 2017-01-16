3522b1289b689e0dcc4655fa16c21a9a4e6c2703
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-01-16 22:21:14 +0100

    Correction de bogues

--- view/admin/viewProfilpromoAdmin.php
+++ view/admin/viewProfilpromoAdmin.php
@@ -5,6 +5,7 @@
 <canvas id="charMAT" width="100%" height="100%" ></canvas>
 <canvas id="charMI" width="100%" height="100%" ></canvas>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 <script>
 	var var_labels = <?php echo json_encode($labels); ?>;
 	var var_profil_ig = <?php echo json_encode($profil_ig, JSON_NUMERIC_CHECK); ?>;
